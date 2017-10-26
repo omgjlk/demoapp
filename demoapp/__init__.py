a265c996d3fb9046c07b8fe215b007b5a51816c7
Jesse Keating <jkeating@j2solutions.net>
2017-10-26 14:16:31 -0700

    Move RESPONSE variable
    When this is a global it's harder to alter when debugging. Just make it
    simple by putting it into the function.

--- demoapp/__init__.py
+++ demoapp/__init__.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 import http.server
 
-RESPONSE='''Hi there!'''
-
 class myHandlers(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
+        RESPONSE='''Hi there!'''
         self.wfile.write(bytes(RESPONSE, 'UTF-8'))
 
 def run(server_class=http.server.HTTPServer, handler_class=myHandlers):
